c0c1828617accbb04196269fa677cc6aadbdd6b1
MantraGitH <camilo.arias56@hotmail.com>
2024-10-24 19:43:35 -0300

    starting 02

---
+++ my-ts-server/app02.ts
@@ -0,0 +1,19 @@
+// Functions and Scope
+
+/*  * - Create examples of basic functions that represent different
+ *   possibilities of the language:
+ *   Without parameters or return value, with one or more parameters, with return value...
+ * - Check if you can create functions within functions.
+ * - Use some examples of functions already available in the language.
+ * - Test the concept of LOCAL and GLOBAL variables.
+ * - You must print the result of all the examples to the console.
+ *   (and keep in mind that each language may have more or fewer possibilities)
+ *
+ * EXTRA DIFFICULTY (optional):
+ * Create a function that receives two string parameters and returns a number.
+ * - The function prints all numbers from 1 to 100, considering that:
+ *   - If the number is a multiple of 3, it shows the string from the first parameter.
+ *   - If the number is a multiple of 5, it shows the string from the second parameter.
+ *   - If the number is a multiple of both 3 and 5, it shows the two strings concatenated.
+ *   - The function returns the number of times the number itself was printed instead of the texts.
+ */
